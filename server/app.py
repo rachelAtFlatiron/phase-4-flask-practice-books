b84e62d4825495932d952934db1e3d31ab6a1b82
Rachel Katz <rachel.katz@flatironschool.com>
2024-01-17 17:05:56 -0500

    demo done

--- server/app.py
+++ server/app.py
@@ -4,7 +4,7 @@ from flask import Flask, make_response, jsonify, request
 from flask_restful import Api, Resource
 from flask_migrate import Migrate
 
-from models import db # import your models here!
+from models import db, Author, Book, Publisher # import your models here!
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -22,6 +22,98 @@ def index():
     return "Hello world"
 
 # write your routes here!
+@app.get('/authors/<int:id>')
+def one_author(id):
+    try:
+        author = Author.query.get(id)
+        if not author:
+            return make_response({
+                'error': 'not found'
+            }, 404)
+        else:
+            # return make_response({
+            #     "id": author.id,
+            #     "name": author.name,
+            #     "pen_name": author.pen_name, 
+            #     "books": [
+            #         book.to_dict() for book in author.books
+            #     ]
+            # }, 200)
+        
+            return make_response({
+                **author.to_dict(), #unpacking key/values
+                # "books": [ #overwriting the books key/value
+                #     book.to_dict() for book in author.books
+                # ]
+            }, 200)
+        
+    except Exception:
+        return make_response({
+            'error': 'something went wrong'
+        }, 500)
 
+@app.delete('/authors/<int:id>')
+def delete_author(id):
+    try:
+        author = Author.query.get(id)
+        if not author:
+            return make_response({'error': 'not found'}, 404)
+        db.session.delete(author)
+        db.session.commit()
+        return make_response({}, 204)
+    except Exception:
+        return make_response({'error': 'something went wront'}, 500)
+    
+@app.get('/books')
+def all_books():
+    try:
+        books = Book.query.all()
+        if not books:
+            return make_response({'error': 'books not found'}, 404)
+        return make_response([book.to_dict() for book in books], 200)
+    except Exception: 
+        return make_response({'error': Exception.__traceback__}, 500)
+
+@app.post('/books')
+def create_book():
+    try: 
+        data = request.get_json()
+        #create class instance of book
+        new_book = Book(
+            title=data['title'],
+            page_count=data['page_count'],
+            author_id=data["author_id"],
+            publisher_id=data["publisher_id"]
+        )
+        db.session.add(new_book)
+        db.session.commit()
+
+        return make_response(new_book.to_dict(), 201)
+    except Exception:
+        return make_response({'error': Exception.__traceback__}, 500)
+    
+@app.get('/publishers/<int:id>')
+def get_publisher(id):
+    pub = Publisher.query.get(id)
+    if not pub: 
+        return make_response({
+            'error': 'not found'
+        }, 404)
+    else:
+        return make_response({
+            **pub.to_dict(rules=('-books', )), # destructuring pub's key/value pairs
+            # -books scraps the entire books list
+
+            #putting back in an authors list that is based on pub.books
+            'authors': [
+                {
+                    'id': book.author.id,
+                    'name': book.author.name,
+                    'pen_name': book.author.pen_name
+                } for book in pub.books
+            ]
+        }, 200)
+
+        
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -11,3 +11,97 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 # write your models here!
+class Book(db.Model, SerializerMixin):
+    __tablename__ = 'books'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False, unique=True)
+    page_count = db.Column(db.Integer)
+
+    author_id = db.Column(db.Integer, db.ForeignKey("authors.id"))
+    publisher_id = db.Column(db.Integer, db.ForeignKey("publishers.id"))
+
+    author = db.relationship('Author', back_populates='books') #Author.books
+    publisher = db.relationship('Publisher', back_populates='books')
+
+    serialize_rules = ('-author.books', '-publisher.books')
+
+    """
+    {
+        ...,
+        author: {
+            ...,
+            books: [
+                {
+                    ...,
+                    author: {
+                        ...,
+                        books: [
+                            ...
+                        ]
+                    }
+                }
+            ]
+        }
+    }
+    """
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "page_count": self.page_count,
+            "author_name": self.author.name,
+            "publisher_name": self.publisher.name
+        }
+    
+    
+    @validates('page_count')
+    def validate_page(self, key, value):
+        if value <= 0:
+            raise ValueError('pages must be greater than 0')
+        else:
+            return value 
+        
+    def __repr__(self):
+        return f'<Book id={self.id} title={self.title} author={self.author.name} />'
+        
+class Publisher(db.Model, SerializerMixin):
+    __tablename__ = "publishers"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    founding_year = db.Column(db.Integer, nullable=False)
+
+    books = db.relationship('Book', back_populates='publisher')
+
+    # Publisher.books -> Book.author -> author instance
+    # books -> author
+    authors = association_proxy('books', 'author')
+
+    serialize_rules = ('-books.publisher', )
+
+    @validates("founding_year")
+    def validate_year(self, key, value):
+        if 1600 <= value <= 2024:
+            return value 
+        else:
+            raise ValueError("year is bad")
+
+    def __repr__(self):
+        return f'<Publisher id={self.id} name={self.name} />'
+
+class Author(db.Model, SerializerMixin):
+    __tablename__ = "authors"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    pen_name = db.Column(db.String)
+
+    books = db.relationship('Book', back_populates='author')  #Book.author
+
+    publishers = association_proxy('books', 'publisher')
+
+    serialize_rules = ('-books.author', )
+
+    def __repr__(self):
+        return f'<Author id={self.id} name={self.name} />'
+  
